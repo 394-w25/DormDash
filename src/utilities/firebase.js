9248970029eda0fc60012bb2cc8321cd487cfebd
Ellie Li <ellieli@dhcp-10-105-96-20.wireless.northwestern.private>
2025-01-08 16:40:16 -0600

    set up firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOHWWkUowBHFyJGNQBY_KKuUepDVjJcIM",
+  authDomain: "dormdash-c26fe.firebaseapp.com",
+  databaseURL: "https://dormdash-c26fe-default-rtdb.firebaseio.com",
+  projectId: "dormdash-c26fe",
+  storageBucket: "dormdash-c26fe.firebasestorage.app",
+  messagingSenderId: "846732477359",
+  appId: "1:846732477359:web:aa0c06dde69aff25c5a55a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
