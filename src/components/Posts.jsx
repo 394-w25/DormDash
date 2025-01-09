011467fc12d9338533357bf21cfcc419059b89bb
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-01-09 16:27:58 -0600

    fix key non-uniqueness; add in exception handling for data

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,7 +2,9 @@ import { useDbData } from "../utilities/firebase.js";
 
 const Posts = () => {
   const [data, error] = useDbData("/");
-  console.log(data);
+  if (error) return <h1>Error loading data: {error.toString()}</h1>;
+  if (data === undefined) return <h1>Loading data...</h1>;
+  if (!data) return <h1>No data found</h1>;
   const allRequests = Object.entries(data.users).flatMap(([userId, user]) =>
     Object.entries(user.requests || {}).map(([requestId, request]) => ({
       userId,
@@ -14,20 +16,21 @@ const Posts = () => {
       description: request.description,
       compensation: request.compensation,
       timestamp: request.timestamp,
-    }
-  )));
+    })),
+  );
 
   return (
     <section className="requests-container">
-      {allRequests.map((request) => (
-            <div key ={request.requestId} className="requests">
-              <h3>{request.title}</h3>
-              <p>{request.location}</p>
-              <p>Requested on: {new Date(request.timestamp).toLocaleString()}</p>
-              <p>Possible Amount Paid: <strong>{request.compensation}</strong></p>
-            </div>
-          ))
-      }
+      {allRequests.map((request, idx) => (
+        <div key={idx} className="requests">
+          <h3>{request.title}</h3>
+          <p>{request.location}</p>
+          <p>Requested on: {new Date(request.timestamp).toLocaleString()}</p>
+          <p>
+            Possible Amount Paid: <strong>{request.compensation}</strong>
+          </p>
+        </div>
+      ))}
     </section>
   );
 };
