15d011c756d7ba4a661750608a551a784832c530
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-01-09 16:28:24 -0600

    change Posts page not-signed-in message

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -3,7 +3,9 @@ import Posts from "../components/Posts.jsx";
 
 const PostsPage = () => {
   const [user] = useAuthState();
-  return <>{user ? <Posts /> : <h1>Sign in first</h1>}</>;
+  return (
+    <>{user ? <Posts /> : <h1>You must be signed in to see this page.</h1>}</>
+  );
 };
 
 export default PostsPage;
