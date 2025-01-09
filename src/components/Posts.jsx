484c60b4ab3a30d37373d84c5e09cb8889581a95
HZZX0318 <zexuanzhou2025@u.northwestern.edu>
2025-01-09 16:09:37 -0600

    Flattened out Json and display as cards

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,7 +3,33 @@ import { useDbData } from "../utilities/firebase.js";
 const Posts = () => {
   const [data, error] = useDbData("/");
   console.log(data);
-  return <>placeholder</>;
+  const allRequests = Object.entries(data.users).flatMap(([userId, user]) =>
+    Object.entries(user.requests || {}).map(([requestId, request]) => ({
+      userId,
+      displayName: user.displayName,
+      email: user.email,
+      requestId,
+      title: request.title,
+      location: request.location,
+      description: request.description,
+      compensation: request.compensation,
+      timestamp: request.timestamp,
+    }
+  )));
+
+  return (
+    <section className="requests-container">
+      {allRequests.map((request) => (
+            <div key ={request.requestId} className="requests">
+              <h3>{request.title}</h3>
+              <p>{request.location}</p>
+              <p>Requested on: {new Date(request.timestamp).toLocaleString()}</p>
+              <p>Possible Amount Paid: <strong>{request.compensation}</strong></p>
+            </div>
+          ))
+      }
+    </section>
+  );
 };
 
 export default Posts;
