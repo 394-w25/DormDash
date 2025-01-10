932e5a6800aaa57c83c0662d371e550c256ffe6e
JoshLiu1015 <yhnujm36@gmail.com>
2025-01-09 21:46:53 -0600

    created ProfilePage and CompletedList

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"; // Add this line
 import HomePage from "../pages/HomePage"; // Ensure the case matches your file name
+import ProfilePage from "../pages/ProfilePage";
 
 const Dispatcher = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
