725cc6f83437d1c3980592a5ed55e25f5620e6ea
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-01-12 18:00:08 -0600

    fix formatting for Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,9 @@ const Post = ({ request }) => {
     <div className="mx-auto w-full bg-slate-300 rounded-2xl p-5 text-center">
       <h3 className="text-3xl font-black">{request.title}</h3>
       <p className="text-2xl">{request.location}</p>
-      <p className="text-2xl">Requested on: {new Date(request.timestamp).toLocaleString()}</p>
+      <p className="text-2xl">
+        Requested on: {new Date(request.timestamp).toLocaleString()}
+      </p>
       <p className="text-2xl">
         Possible Amount Paid: <strong>{request.compensation}</strong>
       </p>
