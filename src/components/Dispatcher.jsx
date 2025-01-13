36956907592ee0b5132c55a708a7ccf065b93e56
JoshLiu1015 <yhnujm36@gmail.com>
2025-01-12 19:20:17 -0600

    added import UserProfile

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import PostPage from "../pages/PostPage";
 import PostsPage from "../pages/PostsPage";
+import ProfilePage from "../pages/ProfilePage";
 
 const Dispatcher = () => {
   return (
