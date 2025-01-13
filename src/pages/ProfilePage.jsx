5113dab889221bd30cb3c8cdc59f3ef2847f42de
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-13 13:34:23 -0600

    refactor to use SignIn component

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,5 @@
-import {
-  signInWithGoogle,
-  firebaseSignOut,
-  useAuthState,
-} from "../utilities/firebase";
+import { firebaseSignOut, useAuthState } from "../utilities/firebase";
+import SignIn from "../components/SignIn";
 
 import CompletedRequests from "../components/CompletedList";
 
@@ -10,21 +7,8 @@ const ProfilePage = () => {
   const [user] = useAuthState();
 
   if (!user) {
-    return (
-      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold text-center">
-          You must be signed in to view this page.
-        </h1>
-        <button
-          onClick={signInWithGoogle}
-          className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition"
-        >
-          Sign in with Google
-        </button>
-      </div>
-    );
+    return <SignIn />;
   }
-
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
       <div className="bg-white w-full max-w-3xl p-6 rounded-lg shadow-lg mb-6">
