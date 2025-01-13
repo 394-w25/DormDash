24fd695d2e79f3a7a087ee29867904bd1691c3af
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-01-12 19:01:31 -0600

    add work-in-progress Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Nav from "./components/Nav";
 import Dispatcher from "./components/Dispatcher";
 
 function App() {
   return (
     <div>
+      <Nav />
       <Dispatcher />
     </div>
   );
---
+++ src/components/nav.jsx
@@ -0,0 +1,19 @@
+const Nav = () => {
+  return (
+    <nav>
+      <div className="w-min ml-auto flex flex-row gap-8 px-48 py-4 text-2xl font-semibold">
+        <a href="/posts" className="hover:underline">
+          Home
+        </a>
+        <a href="/post" className="hover:underline">
+          Create
+        </a>
+        <a href="" className="hover:underline">
+          Profile
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
