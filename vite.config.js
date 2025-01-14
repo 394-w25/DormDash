41974fd97e58c5274dc173d628dadc75cccdbcf4
HZZX0318 <zexuanzhou2025@u.northwestern.edu>
2025-01-13 18:53:25 -0600

    Additional setup for firebase

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/tests/setup.js'],
-  }
+  },
 });
