058cc1ada29bafc196b2ee26b7fc4163835ce436
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-14 16:03:03 -0600

    use custom renderer

--- src/tests/Request.test.jsx
+++ src/tests/Request.test.jsx
@@ -1,5 +1,5 @@
 import { describe } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "./utils.jsx";
 import Request from "../components/Request";
 
 const request = {
