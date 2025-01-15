9b224036e8d6e8a2ef40059552096ea4378e3bda
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-15 10:33:43 -0600

    use modal nested component form; use absolute positioning for close button

--- src/components/RequestModal.jsx
+++ src/components/RequestModal.jsx
@@ -4,11 +4,17 @@ import { Modal } from "@mantine/core";
 
 const RequestModal = ({ opened, onClose, request }) => {
   return (
-    <Modal opened={opened} onClose={onClose} size="lg" centered>
-      <RequestInfo request={request} />
-      <hr className="my-8" />
-      <ContactInfo request={request} />
-    </Modal>
+    <Modal.Root opened={opened} onClose={onClose} size="lg" centered>
+      <Modal.Overlay />
+      <Modal.Content>
+        <Modal.Body>
+          <Modal.CloseButton className="[position:absolute_!important] right-5" />
+          <RequestInfo request={request} />
+          <hr className="my-8" />
+          <ContactInfo request={request} />
+        </Modal.Body>
+      </Modal.Content>
+    </Modal.Root>
   );
 };
 
