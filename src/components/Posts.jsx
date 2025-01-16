2959a02044ad0c1a51f7187455097fa9f91bab47
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-16 17:56:27 -0600

    Revert "Resolve button functionality added"

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,7 @@
-import { useAuthState, useDbData } from "../utilities/firebase.js";
+import { useDbData } from "../utilities/firebase.js";
 import Request from "./Request.jsx";
-import UserRequest from "./UserRequest.jsx";
 
 const Posts = () => {
-  const [user] = useAuthState(); // current user
   const [data, error] = useDbData("/");
   if (error) return <h1>Error loading data: {error.toString()}</h1>;
   if (data === undefined) return <h1>Loading data...</h1>;
@@ -20,23 +18,17 @@ const Posts = () => {
         description: request.description,
         compensation: request.compensation,
         timestamp: request.timestamp,
-        isFulfilled: request.isFulfilled || false,
       })),
     )
-    .filter((request) => !request.isFulfilled) // Filter fulfilled posts here
-    .sort((a, b) => b.timestamp - a.timestamp);
+    .sort((a, b) => b.timestamp - a.timestamp); // Sort by timestamp in descending order
 
   return (
     <>
       <h1 className="text-center text-7xl font-black py-12">Posts</h1>
       <section className="mx-auto w-1/2 gap-8 grid [grid-template-columns:_repeat(auto-fit,_minmax(400px,_1fr))]">
-        {allRequests.map((request, idx) =>
-          request.userId === user?.uid ? (
-            <UserRequest key={idx} request={request} user={user} />
-          ) : (
-            <Request key={idx} request={request} />
-          ),
-        )}
+        {allRequests.map((request, idx) => (
+          <Request key={idx} request={request} />
+        ))}
       </section>
     </>
   );
