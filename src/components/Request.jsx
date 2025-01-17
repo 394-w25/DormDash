ba46618d20164383e4b573f0d400209b4bd1b90b
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-17 16:19:41 -0600

    use EditRequest component

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -1,9 +1,12 @@
+import { useAuthState } from "../utilities/firebase.js";
+import EditRequest from "./EditRequest.jsx";
 import RequestInfo from "./RequestInfo.jsx";
 import RequestModal from "./RequestModal.jsx";
 import { useDisclosure } from "@mantine/hooks";
 
 const Request = ({ request }) => {
   const [opened, { open, close }] = useDisclosure(false);
+  const [user] = useAuthState();
   return (
     <>
       <RequestModal opened={opened} onClose={close} request={request} />
@@ -11,6 +14,7 @@ const Request = ({ request }) => {
         onClick={open}
         className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow border border-gray-200"
       >
+        {user?.uid === request?.userId && <EditRequest request={request} />}
         <RequestInfo request={request} />
       </div>
     </>
