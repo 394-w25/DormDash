69a4b4041b1fc781b078dfd524db76596222ad8b
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-17 16:19:33 -0600

    add EditRequest component stub

---
+++ src/components/EditRequest.jsx
@@ -0,0 +1,28 @@
+import { Button, Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import RequestForm from "./RequestForm";
+
+const EditRequest = ({ request }) => {
+  const [opened, { open, close }] = useDisclosure(false);
+  const handleClick = (e) => {
+    e.stopPropagation();
+    console.log(e);
+    open();
+  };
+  return (
+    <>
+      <Modal.Root opened={opened} onClose={close} size="lg" centered>
+        <Modal.Overlay onClick={(e) => e.stopPropagation()} />
+        <Modal.Content>
+          <Modal.Body>
+            <Modal.CloseButton className="[position:absolute_!important] right-5" />
+            <RequestForm />
+          </Modal.Body>
+        </Modal.Content>
+      </Modal.Root>
+      <Button onClick={handleClick}>EDIT</Button>
+    </>
+  );
+};
+
+export default EditRequest;
