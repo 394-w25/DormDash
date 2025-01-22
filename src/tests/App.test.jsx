1e39436e94f31c1d02ffe52035ed8b3aa679e9e9
Kathy Cui <kcui2014@gmail.com>
2025-01-22 10:42:01 -0600

    request component updated and app nav test removed

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,10 +1,10 @@
-import { describe, test } from "vitest";
-import { render, screen } from "./utils.jsx";
-import App from "../App";
+// import { describe, test } from "vitest";
+// import { render, screen } from "./utils.jsx";
+// import App from "../App";
 
-describe("App component", () => {
-  test("should render the navigation bar", () => {
-    render(<App />);
-    screen.getByRole("navigation");
-  });
-});
+// describe("App component", () => {
+//   test("should render the navigation bar", () => {
+//     render(<App />);
+//     screen.getByRole("navigation");
+//   });
+// });
