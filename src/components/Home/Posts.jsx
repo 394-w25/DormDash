a9c8fbc71fa6696fd71ebc450b8039e75e07a7fb
HZZX0318 <zexuanzhou2025@u.northwestern.edu>
2025-02-02 14:52:57 -0600

    added comments for previous pr

--- src/components/Home/Posts.jsx
+++ src/components/Home/Posts.jsx
@@ -20,12 +20,15 @@ const Posts = ({
 
   // Filter requests based on lifted state
   const filteredRequests = allRequests.filter((request) => {
+    // first filter through tags
     const matchesTags =
       selectedTags.length === 0 ||
       selectedTags.every((tag) => request.tags.includes(tag));
+    // then filter through compensation range
     const matchesCompensation =
       request.compensation >= minCompensation &&
       (maxCompensation === "" || request.compensation <= maxCompensation);
+    // then filter through title and desciption if user used search
     const matchesSearch =
       request.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
       request.description.toLowerCase().includes(searchQuery.toLowerCase());
@@ -37,12 +40,14 @@ const Posts = ({
     );
   });
 
+  // sort the posts for display
   const sortedRequests = [...filteredRequests].sort((a, b) => {
     const field = sortField === "compensation" ? "compensation" : "timestamp";
     const orderMultiplier = sortOrder === "asc" ? 1 : -1;
     return (a[field] - b[field]) * orderMultiplier;
   });
 
+  // all the tags that currently exist in the database
   const allTags = [...new Set(allRequests.flatMap((request) => request.tags))];
 
   return (
