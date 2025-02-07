9992f3e5a68d241e3ab5e8ade50bd45b71f97a22
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-02-07 15:21:32 -0600

    add Notifications component to testing setup

--- src/tests/utils.jsx
+++ src/tests/utils.jsx
@@ -2,12 +2,14 @@ import { render } from "@testing-library/react";
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import "@mantine/core/styles.css";
+import { Notifications } from "@mantine/notifications";
 
 // https://testing-library.com/docs/react-testing-library/setup/#custom-render
 const customRender = (ui) =>
   render(<>{ui}</>, {
     wrapper: ({ children }) => (
       <MantineProvider>
+        <Notifications />
         <ModalsProvider>{children}</ModalsProvider>
       </MantineProvider>
     ),
