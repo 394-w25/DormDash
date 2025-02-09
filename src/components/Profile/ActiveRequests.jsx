d41deb05840ea2431eb91a7e652980ef36a7b6fd
JoshLiu1015 <yhnujm36@gmail.com>
2025-02-09 14:08:25 -0600

    removed commented-out code and burger menu

--- src/components/Profile/ActiveRequests.jsx
+++ src/components/Profile/ActiveRequests.jsx
@@ -36,31 +36,4 @@ function ActiveRequests() {
   );
 }
 
-// function ActiveRequests() {
-//   const [data, error] = useDbData("/");
-//   const [user] = useAuthState();
-
-//   if (error) return <h1>Error loading data: {error.toString()}</h1>;
-//   if (data === undefined) return <h1>Loading data...</h1>;
-//   if (!data) return <h1>No data found</h1>;
-
-//   const activeRequests = getRequests(data, {
-//     userFilter: (userId) => userId === user.uid,
-//     requestFilter: (request) => !request.isFulfilled,
-//   });
-//   return (
-//     <>
-//       {activeRequests.length > 0 ? (
-//         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-//           {activeRequests.map((request) => (
-//             <Request request={request} />
-//           ))}
-//         </div>
-//       ) : (
-//         <p className="text-gray-500 text-start">You have no active requests.</p>
-//       )}
-//     </>
-//   );
-// }
-
 export default ActiveRequests;
